fe19850c6b986f0939dd97cab9adf8acf3d6b8b1
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-03-06 22:38:41 +0100

    add addEventListener to each link for remove active class from navbar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var hamburger = document.querySelector('.hamburger');
 var navBar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-links ul li')
 
 hamburger.addEventListener('click', () => {
   navBar.classList.toggle('active');
 })
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navBar.classList.remove('active');
+  })
+});
+
+console.log(navLinks)
