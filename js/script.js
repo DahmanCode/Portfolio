38885b4bc51ce116c28ac0d08ae4384623c35d02
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-05-17 00:00:31 +0100

    add new project 'Book-Author website'

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ navLinks.forEach(link => {
 
 console.log(navLinks)
 
+// ===== Unclickable Button ===== //
+
+// const buttonHeight = 50
+// const buttonWidth = 150
+
+// const maxWidth = window.innerWidth - buttonWidth
+// const maxHeight = window.innerHeight - buttonHeight
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const button = document.getElementById('button')
+
+//   button.addEventListener('mouseover', () => {
+//     button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px'
+//     button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px'
+//   })
+// })
